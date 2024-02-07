a467de1a305b0201b91faa8e236dfa5ee4d49f7b
vincent0712 <vincent.jonsson@elev.ga.lbs.se>
2024-02-07 11:52:23 +0100

    asd
    as

--- Assets/Scriptes/MeleeAttack.cs
+++ Assets/Scriptes/MeleeAttack.cs
@@ -5,26 +5,8 @@ using UnityEngine.Events;
 
 public class MeleeAttack : MonoBehaviour
 {
-    public GameObject TriUp;
-    public GameObject TriDown;
-    public GameObject TriLeft;
-    public GameObject TriRight;
-    public UnityEvent onTakeDamage;
-    private EnemyHealth enemyHealth;
-    public int Enemyhealth = 50;
-    public bool isDead = false;
-    public UnityEvent onDeath;
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.tag == "Enemy" && Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            Enemyhealth -= 20;
-            if(Enemyhealth <= 0)
-            {
-                isDead = true;
-                onDeath.Invoke();
-            }
-            return;
-        }
-    }
+
+
+
+
 }
